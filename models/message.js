cbbb4ec5359df1d6ade48e69929adac23516ddcc
Matthew Broatch <mnbroatch@gmail.com>
2016-07-16 13:00:49 -0700

    lets see michael

--- models/message.js
+++ models/message.js
@@ -1,5 +1,14 @@
 const mongoose = require('mongoose');
 const request = require('request');
+const watson = require('watson-developer-cloud');
+const fs = require('fs');
+
+const textToSpeech = watson.text_to_speech({
+  username: "c3fa4ff8-b7ff-4c70-aae0-50017dc58216",
+  password: "lCioWmy44BQi",
+  // url: "https://stream.watsonplatform.net/text-to-speech/api",
+  version: "v1",
+});
 
 const messageSchema = new mongoose.Schema({
   author: { type: String },
@@ -18,11 +27,22 @@ messageSchema.pre('save', function (next) {
     },
   }
   request(options, (err,response,body) => {
+    if (err) return err;
     this.body = body;
-    console.log('this',this);
+    this.toSpeech(this.body);
     next();
   });
 });
+
+messageSchema.methods.toSpeech = function () {
+console.log('this.body',this.body);
+  const params = {
+    text:this.body,
+    voice: "en-US_MichaelVoice",
+    accept: "audio/wav",
+  };
+  textToSpeech.synthesize(params).pipe(fs.createWriteStream('output.wav'));
+};
 
 
 const message = mongoose.model('Message', messageSchema);
